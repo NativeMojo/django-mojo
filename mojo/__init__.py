c8d6b9f8be8db8ff25aabb7b302669501ba59173
IamMojo <ian@nativemojo.com>
2026-08-20 21:41:49 -0700

    Release 1.15.12

--- mojo/__init__.py
+++ mojo/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.15.11"
+__version__ = "1.15.12"
 
 # THIS LINE IS ON THE NODE-BOOTSTRAP PATH AND MUST STAY SETTINGS-FREE.
 # `python3 -m mojo.deploy.config_sync` imports this package before it can parse
